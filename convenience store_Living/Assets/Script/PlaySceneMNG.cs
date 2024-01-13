e48be85b3ed6baf4ea94c9a7fc742df5f79fd2d4
Hognshi <123186080+Hognshi@users.noreply.github.com>
2024-01-14 01:37:58 +0900

    game Time to random memo
    인게임 내 시간 구현 및
    
    랜덤으로 금일 주의사항
    메모에 생성

---
+++ convenience store_Living/Assets/Script/PlaySceneMNG.cs
@@ -0,0 +1,100 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlaySceneMNG : MonoBehaviour
+{
+    public Button memoBTN;
+    public Text workTimeText;
+    public Text memoText;
+    public GameObject memoList; // 메모를 띄울 공간
+
+    [SerializeField]
+    float sec;
+    [SerializeField]
+    int min;
+    int workTime = 1; //게임중 몇일차인지
+    int rand;
+
+    void Start()
+    {
+        memoBTN.onClick.AddListener(OpenMemo);
+    }
+
+
+    void Update()
+    {
+        GameTime();
+        EnterEscape();
+        TodaysWarning();
+    }
+
+    public void OpenMemo()
+    {
+        memoList.SetActive(true);
+    }
+
+    void EnterEscape() //esc눌렀을때 꺼짐
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            memoList.SetActive(false);
+        }
+    }
+
+    void GameTime()
+    {
+        sec += Time.deltaTime;
+
+        if ((int)sec == 60)
+        {
+            min++;
+            sec = 0;
+        }
+
+        if (min == 5)
+        {
+            min = 0;
+            workTime++;
+            rand = Random.Range(0, 3);
+        }
+        workTimeText.text = workTime.ToString();
+    }
+
+    void TodaysWarning() // 메모에 적히는 금일 주의사항
+    {
+
+        if (workTime >= 15)
+        {
+            switch (rand)
+            {
+                case 0:
+                    memoText.text = "*민증v\n*운전면허증\n*푸키먼 빵o";
+                    break;
+                case 1:
+                    memoText.text = "*민증v\n*운전면허증\n*담배 재고x";
+                    break;
+                case 2:
+                    memoText.text = "*민증V\n*운전면허증\n*신선식품 재고x";
+                    break;
+            }
+        }
+        else if (workTime < 15)
+        {
+            switch (rand)
+            {
+                case 0:
+                    memoText.text = "*민증v\n*푸키먼 빵o";
+                    break;
+                case 1:
+                    memoText.text = "*민증v\n*담배 재고x";
+                    break;
+                case 2:
+                    memoText.text = "*민증v\n*신선식품 재고x";
+                    break;
+            }
+        }
+    }
+}
